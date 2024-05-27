292df3567c2efc08dba84d12adafa336f99f8f7e
ohtak6843_Desktop <ohtak6843@gmail.com>
2024-05-28 00:06:07 +0900

    GUI 제작 시작

---
+++ MainGUI.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import messagebox
+
+class MainGUI():
+    def __init__(self):
+        self.window = Tk()
+        self.window.title('TMG')
+        self.window.geometry('800x600')
+
+        self.window.mainloop()
+
+
+MainGUI()
