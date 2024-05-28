0d7c8366e614b040aeb1c3f91246cadfadfb74f7
ohtak6843_Laptop <ohtak6843@gmail.com>
2024-05-28 11:18:29 +0900

    검색 시 자료에 GUI에 출력

--- SearchGUI.py
+++ SearchGUI.py
@@ -20,11 +20,28 @@ class SearchGUI:
                               command=self.pressedSearchB)
         self.searchB.place(x=450, y=20, height=30)
 
+        self.nameLabel = Label(self.window, text="", width=24, height=1, font=self.fontstyle2,
+                               highlightcolor='black', highlightbackground='black', highlightthickness=2)
+        self.nameLabel.place(x=10, y=120)
+        self.levelLabel = Label(self.window, text="", width=24, height=1, font=self.fontstyle2,
+                                highlightcolor='black', highlightbackground='black', highlightthickness=2)
+        self.levelLabel.place(x=10, y=160)
+        self.serverLabel = Label(self.window, text="", width=24, height=1, font=self.fontstyle2,
+                                 highlightcolor='black', highlightbackground='black', highlightthickness=2)
+        self.serverLabel.place(x=10, y=200)
+        self.cImageLabel = Label(self.window, image=None, highlightcolor='black', highlightbackground='black',
+                                 highlightthickness=2, )
+        self.cImageLabel.place(x=500, y=100, width=160, height=160)
+
         self.window.mainloop()
 
     def pressedSearchB(self):
         self.mapleInfo = MapleInfo(self.searchE.get())
-        print(self.mapleInfo.basic)
+        if self.mapleInfo.ocid != None:
+            self.nameLabel.configure(text=self.mapleInfo.name)
+            self.levelLabel.configure(text=self.mapleInfo.basic['character_name'])
+            self.serverLabel.configure(text=self.mapleInfo.basic['world_name'])
+            self.cImageLabel.configure(image=self.mapleInfo.basic['character_image'])
 
 
 if __name__ == "__main__":
