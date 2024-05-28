10a8f4f190f10ff1e85e724d8f5a61a2b559385c
ohtak6843_Laptop <ohtak6843@gmail.com>
2024-05-28 17:08:23 +0900

    이미지 출력 테스트 파일

---
+++ test.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from io import BytesIO
+import urllib
+import urllib.request
+from PIL import Image, ImageTk
+
+window = Tk()
+window.geometry('500x500+500+200')
+url = 'http://tong.visitkorea.or.kr/cms/resource/74/2396274_image2_1.JPG'
+with urllib.request.urlopen(url) as u:
+    raw_data = u.read()
+im = Image.open(BytesIO(raw_data))
+image = ImageTk.PhotoImage(im)
+Label(window, image=image, height=400, width=400).pack()
+
+window.mainloop()
