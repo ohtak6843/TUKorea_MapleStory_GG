5d57806cc4e69ce3e56e6d0a2df3a3e1d5add5bb
ohtak6843_Laptop <ohtak6843@gmail.com>
2024-06-06 15:50:37 +0900

    tkinterMapView 사용해보기

--- MapGUI.py
+++ MapGUI.py
@@ -1,4 +1,26 @@
+from tkinter import *
+from tkinter import messagebox
+from tkintermapview import TkinterMapView
+
+
 class MapGUI:
-    def __init__(self, width=800, height=800):
-        self.width = width
-        self.height = height
+    def __init__(self):
+        self.window = Tk()
+        self.window.title('TMG')
+        self.window.geometry('800x800')
+
+        self.gmap_widget = TkinterMapView(self.window, width=750, height=600)
+        self.gmap_widget.pack(fill='both')
+
+        self.gmap_widget.set_tile_server("https://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga",
+                                         max_zoom=22)  # google normal
+
+        self.marker = self.gmap_widget.set_address("KFC 10421 South Western Avenue", marker=True)
+        self.marker.set_text(text="My Favorite KFC")
+        self.gmap_widget.set_zoom(12)
+
+        self.window.mainloop()
+
+
+if __name__ == "__main__":
+    MapGUI()
