a347befbc21a56bba1d23543bbee6e58deb30ed3
ohtak6843_Desktop <ohtak6843@gmail.com>
2024-06-07 06:06:55 +0900

    pc방 데이터 읽어오기 성공

--- MapGUI.py
+++ MapGUI.py
@@ -1,26 +1,154 @@
-from tkinter import *
-from tkinter import messagebox
+import sys
+import tkinter
+import tkinter.messagebox
 from tkintermapview import TkinterMapView
 
+import requests
 
-class MapGUI:
-    def __init__(self):
-        self.window = Tk()
-        self.window.title('TMG')
-        self.window.geometry('800x800')
 
-        self.gmap_widget = TkinterMapView(self.window, width=750, height=600)
-        self.gmap_widget.pack(fill='both')
+class App(tkinter.Tk):
+    APP_NAME = "map_view_demo.py"
+    WIDTH = 800
+    HEIGHT = 750
 
-        self.gmap_widget.set_tile_server("https://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga",
-                                         max_zoom=22)  # google normal
+    def __init__(self, *args, **kwargs):
+        tkinter.Tk.__init__(self, *args, **kwargs)
 
-        self.marker = self.gmap_widget.set_address("KFC 10421 South Western Avenue", marker=True)
-        self.marker.set_text(text="My Favorite KFC")
-        self.gmap_widget.set_zoom(12)
+        self.title(self.APP_NAME)
+        self.geometry(f"{self.WIDTH}x{self.HEIGHT}")
 
-        self.window.mainloop()
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.bind("<Return>", self.search)
+
+        if sys.platform == "darwin":
+            self.bind("<Command-q>", self.on_closing)
+            self.bind("<Command-w>", self.on_closing)
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=0)
+        self.grid_columnconfigure(2, weight=0)
+        self.grid_rowconfigure(1, weight=1)
+
+        self.search_bar = tkinter.Entry(self, width=50)
+        self.search_bar.grid(row=0, column=0, pady=10, padx=10, sticky="we")
+        self.search_bar.focus()
+
+        self.search_bar_button = tkinter.Button(master=self, width=8, text="Search", command=self.search)
+        self.search_bar_button.grid(row=0, column=1, pady=10, padx=10)
+
+        self.search_bar_clear = tkinter.Button(master=self, width=8, text="Clear", command=self.clear)
+        self.search_bar_clear.grid(row=0, column=2, pady=10, padx=10)
+
+        self.map_widget = TkinterMapView(width=self.WIDTH, height=600, corner_radius=0)
+        self.map_widget.grid(row=1, column=0, columnspan=3, sticky="nsew")
+
+        self.marker_list_box = tkinter.Listbox(self, height=8)
+        self.marker_list_box.grid(row=2, column=0, columnspan=1, sticky="ew", padx=10, pady=10)
+
+        self.listbox_button_frame = tkinter.Frame(master=self)
+        self.listbox_button_frame.grid(row=2, column=1, sticky="nsew", columnspan=2)
+
+        self.listbox_button_frame.grid_columnconfigure(0, weight=1)
+
+        self.save_marker_button = tkinter.Button(master=self.listbox_button_frame, width=20, text="save current marker",
+                                                 command=self.save_marker)
+        self.save_marker_button.grid(row=0, column=0, pady=10, padx=10)
+
+        self.clear_marker_button = tkinter.Button(master=self.listbox_button_frame, width=20, text="clear marker list",
+                                                  command=self.clear_marker_list)
+        self.clear_marker_button.grid(row=1, column=0, pady=10, padx=10)
+
+        self.connect_marker_button = tkinter.Button(master=self.listbox_button_frame, width=20,
+                                                    text="connect marker with path",
+                                                    command=self.connect_marker)
+        self.connect_marker_button.grid(row=2, column=0, pady=10, padx=10)
+
+        self.map_widget.set_address("NYC")
+
+        self.marker_list = []
+        self.marker_path = None
+
+        self.search_marker = None
+        self.search_in_progress = False
+
+        self.mainloop()
+
+    def search(self, event=None):
+        if not self.search_in_progress:
+            self.search_in_progress = True
+            if self.search_marker not in self.marker_list:
+                self.map_widget.delete(self.search_marker)
+
+            address = self.search_bar.get()
+            self.search_marker = self.map_widget.set_address(address, marker=True)
+            if self.search_marker is False:
+                # address was invalid (return value is False)
+                self.search_marker = None
+            self.search_in_progress = False
+
+    def save_marker(self):
+        if self.search_marker is not None:
+            self.marker_list_box.insert(tkinter.END, f" {len(self.marker_list)}. {self.search_marker.text} ")
+            self.marker_list_box.see(tkinter.END)
+            self.marker_list.append(self.search_marker)
+
+    def clear_marker_list(self):
+        for marker in self.marker_list:
+            self.map_widget.delete(marker)
+
+        self.marker_list_box.delete(0, tkinter.END)
+        self.marker_list.clear()
+        self.connect_marker()
+
+    def connect_marker(self):
+        print(self.marker_list)
+        position_list = []
+
+        for marker in self.marker_list:
+            position_list.append(marker.position)
+
+        if self.marker_path is not None:
+            self.map_widget.delete(self.marker_path)
+
+        if len(position_list) > 0:
+            self.marker_path = self.map_widget.set_path(position_list)
+
+    def clear(self):
+        self.search_bar.delete(0, last=tkinter.END)
+        self.map_widget.delete(self.search_marker)
+
+    def on_closing(self, event=0):
+        self.destroy()
+        exit()
 
 
 if __name__ == "__main__":
-    MapGUI()
+    # app = App()
+
+    # 공공데이터 API 키
+    api_key = 'PKGFCGC1GAJJBXm5h13NotLREkhRZqXdj3rBL7VIoq+cZa9ub4MXTnCiZNYRk9StuDZfvgMZ7oLn3BKVZV2OPg=='
+
+    # 서울시 용산구 PC방 현황
+    url = "https://api.odcloud.kr/api/3077839/v1/uddi:20f437b7-92fe-4624-8c5a-b2f136085bfe"
+    params = {
+        "serviceKey": api_key,
+        "page": 1,
+        "perpage": 100,
+        "returnType": "JSON"
+    }
+    response = requests.get(url, params=params)
+    items = response.json()['data']
+
+    pc_rooms = []
+    for item in items:
+        pc_room = {
+            "연번": item['연번'],
+            "업종명": item['업종명'],
+            "상호": item['상호'],
+            "영업소소재지(도로명)": item['영업소소재지(도로명)'],
+            "데이터기준일자": item['데이터기준일자']
+        }
+        pc_rooms.append(pc_room)
+
+    for pc_room in pc_rooms:
+        print(pc_room)
