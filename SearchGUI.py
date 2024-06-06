039f94ca497e84a3a91565e81fe89498ec111d8e
Lucianne0424 <pwj0424@naver.com>
2024-06-06 14:51:07 +0900

    character Info창 정보 출력 완료

--- SearchGUI.py
+++ SearchGUI.py
@@ -44,6 +44,10 @@ class SearchGUI:
         self.searchB.place(x=self.xSize//2 + 100 - 15, y=3,width=100, height=30)
 
 
+        cImageLabel_bg = Label(self.window, bg="#c9ced0", width=30, height=11)
+        cImageLabel_bg.place(x=195, y=37)
+
+
         """# 이메일 전송창
         self.mailSendE = Entry(self.window, highlightcolor='black', highlightbackground='black', highlightthickness=2)
         self.mailSendE.place(x=self.xSize//2 - 150 - 35, y=self.ySize - 35, width=300, height=30)
@@ -53,7 +57,7 @@ class SearchGUI:
         self.mailSendB.place(x=self.xSize//2 + 150 - 35, y=self.ySize - 35, width=100, height=30)"""
 
 
-        #self.characterInfoSearch() # 테스트 출력
+        # self.characterInfoSearch() # 테스트 출력
         self.window.mainloop()
 
 
@@ -67,43 +71,43 @@ class SearchGUI:
         self.characterInfo_label.place(x=0, y=0)
 
     def characterInfoSearch(self):
+
         # 직업 리벨
-        self.nameLabel = Label(self.window, text=str(self.mapleInfo.basic["character_class"]), bg='#9aa2ab', width=15)
-        self.nameLabel.place(x=40, y=43)
+        self.classLabel = Label(self.window, text=str(self.mapleInfo.basic["character_class"]), bg='#9aa2ab', width=15)
+        self.classLabel.place(x=40, y=43)
 
         # 유니온
         self.unionLabel = Label(self.window, text=str(self.mapleInfo.union["union_level"]), bg='#c9ced0', width=5)
         self.unionLabel.place(x=115, y=135)
 
         # 무릉도장
-        self.Mu_Lung_Dojo = Label(self.window, text="", bg='#c9ced0', width=5)
+        self.Mu_Lung_Dojo = Label(self.window, text=str(self.mapleInfo.Mu_Lung_Dojo["dojang_best_floor"]), bg='#c9ced0', width=5)
         self.Mu_Lung_Dojo.place(x=115, y=158)
 
         # 인기도
-        self.unionLabel = Label(self.window, text="", bg='#c9ced0', width=5)
+        self.unionLabel = Label(self.window, text=str(self.mapleInfo.popularity["popularity"]), bg='#c9ced0', width=5)
         self.unionLabel.place(x=115, y=183)
 
 
         # 길드
-        self.guild = Label(self.window, text="", bg='#c9ced0', width=10)
-        self.guild.place(x=493  , y=159)
+        self.guildLabel = Label(self.window, text=str(self.mapleInfo.basic["character_guild_name"]), bg='#c9ced0', width=10)
+        self.guildLabel.place(x=493  , y=182)
 
-        # 연맹
-        self.Federation = Label(self.window, text="", bg='#c9ced0', width=10)
-        self.Federation.place(x=493, y=183)
+        # 이미지 출력
+        self.cImageLabel = Label(self.window, bg="#c9ced0")
+        self.cImageLabel.place(x=255, y=65)
 
-        # self.nameLabel.configure(text="이름: " + self.mapleInfo.name)
+        # 이름 출력
+        self.nameLabel = Label(self.window, bg="#9aa2ab", text=str(self.mapleInfo.name), width=15)
+        self.nameLabel.place(x=245, y=170)
 
-
-        # self.levelLabel.configure(text="레벨: " + str(self.mapleInfo.basic['character_level']))
-        # self.serverLabel.configure(text="서버: " + self.mapleInfo.basic['world_name'])
-        # url = self.mapleInfo.basic['character_image']
-        # with urllib.request.urlopen(url) as u:
-        #     raw_data = u.read()
-        # im = Image.open(BytesIO(raw_data))
-        # image = ImageTk.PhotoImage(im, master=self.window)
-        # self.cImageLabel.configure(image=image)
-        # self.cImageLabel.image = image
+        url = self.mapleInfo.basic['character_image']
+        with urllib.request.urlopen(url) as u:
+            raw_data = u.read()
+        im = Image.open(BytesIO(raw_data))
+        image = ImageTk.PhotoImage(im, master=self.window)
+        self.cImageLabel.configure(image=image)
+        self.cImageLabel.image = image
 
     def bar_print(self): # 찾고자 하는 캐릭 정보를 선택하는 부분 배경 출력
         tempImage = Image.open("image/bar.png")
